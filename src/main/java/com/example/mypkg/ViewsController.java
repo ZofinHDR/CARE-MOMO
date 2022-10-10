906f1da033c235e0cb88a4f685450ec0c4c5399c
ZofinHDR <marc.heimann98@hotmail.com>
2022-10-10 19:31:28 +0200

    initialisierung

---
+++ src/main/java/com/example/mypkg/ViewsController.java
@@ -0,0 +1,34 @@
+package com.example.mypkg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//Controller for all html views
+@Controller
+public class ViewsController {
+
+	@GetMapping("/homeUserView")
+	public String homeUserView() {
+		return "homeUserView";
+	}
+
+	@GetMapping("/infoUserView")
+	public String infoUserView() {
+		return "infoUserView";
+	}
+
+	@GetMapping("/socialUserView")
+	public String socialUserView() {
+		return "socialUserView";
+	}
+
+	@GetMapping("/transparencyUserView")
+	public String transparencyUserView() {
+		return "transparencyUserView";
+	}
+
+	@GetMapping("/functionalityUserView")
+	public String functionalityUserView() {
+		return "functionalityUserView";
+	}
+}
