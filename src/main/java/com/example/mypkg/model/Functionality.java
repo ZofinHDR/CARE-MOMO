cc4c09905d816def0742c2b15c2627b2ebd507af
ZofinHDR <marc.heimann98@hotmail.com>
2022-10-13 20:25:47 +0200

    wrote getter setter for Transparency.java & Functionality.java

--- src/main/java/com/example/mypkg/model/Functionality.java
+++ src/main/java/com/example/mypkg/model/Functionality.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class Functionality {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Boolean fq1;
     private Boolean fq2;
@@ -18,4 +18,61 @@ public class Functionality {
     private Boolean fq4;
     private String fq5;
     private String fq6;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setFq1(Boolean fq1) {
+        this.fq1 = fq1;
+    }
+
+    public Boolean getFq1() {
+        return fq1;
+    }
+
+    public void setFq2(Boolean fq2) {
+        this.fq2 = fq2;
+    }
+
+    public Boolean getFq2() {
+        return fq2;
+    }
+
+    public void setFq3(Boolean fq3) {
+        this.fq3 = fq3;
+    }
+
+    public Boolean getFq3() {
+        return fq3;
+    }
+
+    public void setFq4(Boolean fq4) {
+        this.fq4 = fq4;
+    }
+
+    public Boolean getFq4() {
+        return fq4;
+    }
+
+    public void setFq5(String fq5) {
+        this.fq5 = fq5;
+    }
+
+    public String getFq5() {
+        return fq5;
+    }
+
+    public void setFq6(String fq6) {
+        this.fq6 = fq6;
+    }
+
+    public String getFq6() {
+        return fq6;
+    }
 }
+
--- src/main/java/com/example/mypkg/model/Transparency.java
+++ src/main/java/com/example/mypkg/model/Transparency.java
@@ -16,4 +16,44 @@ public class Transparency {
     private Boolean tq2;
     private Boolean tq3;
     private Boolean tq4;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setTq1(Boolean tq1) {
+        this.tq1 = tq1;
+    }
+
+    public Boolean getTq1() {
+        return tq1;
+    }
+
+    public void setTq2(Boolean tq2) {
+        this.tq2 = tq2;
+    }
+
+    public Boolean getTq2() {
+        return tq2;
+    }
+
+    public void setTq3(Boolean tq3) {
+        this.tq3 = tq3;
+    }
+
+    public Boolean getTq3() {
+        return tq3;
+    }
+
+    public void setTq4(Boolean tq4) {
+        this.tq4 = tq4;
+    }
+
+    public Boolean getTq4() {
+        return tq4;
+    }
 }
